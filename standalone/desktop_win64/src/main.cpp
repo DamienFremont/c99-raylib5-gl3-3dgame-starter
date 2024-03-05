f963169e919af592b8a06b1d5039af815fd3b9eb
Damien FREMONT <damien.fremont@gmail.com>
2024-03-05 21:28:56 +0100

    feat: 🎸 monorepo

---
+++ standalone/desktop_win64/src/main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <shaders_postprocessing.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine, int nCmdShow)
+{
+    return main();
+}
