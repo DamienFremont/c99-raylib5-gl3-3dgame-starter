6915533508e03896a9ce72fc52deef14d2556572
Damien FREMONT <damien.fremont@gmail.com>
2024-04-19 23:08:54 +0200

    fix: 🐛 tmp var - shaders

--- platform/engine/src/eng_material.c
+++ platform/engine/src/eng_material.c
@@ -12,9 +12,9 @@ Shader TileTexture2D(Vector2 tiling)
 {
     // Set the texture tiling using a shader
     float tilingArr[2] = {tiling.x, tiling.y};
-    char fs_path[PATH_MAX];
-    GetAssetPath(fs_path, "resources/shaders/glsl%i/tiling.fs");
-    Shader shader = LoadShader(0, fs_path);
+    Shader shader = LoadShader_GetShaderPath(
+        0,
+        "resources/shaders/glsl%i/tiling.fs");
     SetShaderValue(shader, GetShaderLocation(shader, "tiling"), tilingArr, SHADER_UNIFORM_VEC2);
     return shader;
 }
--- platform/engine/src/eng_skybox.c
+++ platform/engine/src/eng_skybox.c
@@ -9,23 +9,20 @@
 
 Model LoadSkyboxFromImage(Image img)
 {
-    char tmp[PATH_MAX];
-    char tmp2[PATH_MAX];
-
     Mesh cube = GenMeshCube(500.0f, 500.0f, 500.0f);
     Model skybox = LoadModelFromMesh(cube);
     // Load skybox shader and set required locations
     // NOTE: Some locations are automatically set at shader loading
-    skybox.materials[0].shader = LoadShader(
-        GetShaderPath(tmp, "resources/shaders/glsl%i/skybox.vs"),
-        GetShaderPath(tmp2, "resources/shaders/glsl%i/skybox.fs"));
+    skybox.materials[0].shader = LoadShader_GetShaderPath(
+        "resources/shaders/glsl%i/skybox.vs",
+        "resources/shaders/glsl%i/skybox.fs");
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "environmentMap"), (int[1]){MATERIAL_MAP_CUBEMAP}, SHADER_UNIFORM_INT);
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "doGamma"), (int[1]){0}, SHADER_UNIFORM_INT);
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "vflipped"), (int[1]){0}, SHADER_UNIFORM_INT);
     // Load cubemap shader and setup required shader locations
-    Shader shdrCubemap = LoadShader(
-        GetShaderPath(tmp, "resources/shaders/glsl%i/cubemap.vs"),
-        GetShaderPath(tmp2, "resources/shaders/glsl%i/cubemap.fs"));
+    Shader shdrCubemap = LoadShader_GetShaderPath(
+        "resources/shaders/glsl%i/cubemap.vs",
+        "resources/shaders/glsl%i/cubemap.fs");
     SetShaderValue(shdrCubemap, GetShaderLocation(shdrCubemap, "equirectangularMap"), (int[1]){0}, SHADER_UNIFORM_INT);
     // Load img texture
     skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT); // CUBEMAP_LAYOUT_PANORAMA
