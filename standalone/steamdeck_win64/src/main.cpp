90f3c79199cb0a8abbb326cc75a888f4da251a29
Damien FREMONT <damien.fremont@gmail.com>
2024-06-27 15:29:55 +0200

    feat: 🎸 steamdeck - added cmake module

--- standalone/steamdeck_win64/src/main.cpp
+++ standalone/steamdeck_win64/src/main.cpp
@@ -43,10 +43,10 @@ AppConfiguration InitConfig()
     appConfig.glsl_version = 330;
     appConfig.screen_width = ENG_GetScreenWidth(HD);
     appConfig.screen_height = ENG_GetScreenHeight(HD);
-    appConfig.fps_limit = 0;
+    appConfig.fps_limit = 60;
     appConfig.fps_counter_show = true;
     appConfig.postpro_antialias_msaa = true;
-    appConfig.postpro_texturefilter = 3;
+    appConfig.postpro_texturefilter = 1;
     appConfig.postpro_effect_bloom = true;
     return appConfig;
 }
@@ -56,7 +56,7 @@ char* GetExecutionPath()
     char res[999];
     std::string str = _pgmptr;
     int exepath_len = str.size();
-    std::string exebin = "\\desktop_win64.exe";
+    std::string exebin = "\\steamdeck_win64.exe";
     int exebin_len = exebin.size();
     std::string str2 = str.substr(0, exepath_len - exebin_len);
     strcpy(res, str2.c_str());
