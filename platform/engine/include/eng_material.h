d416149554dfef54ccef4256e18639d228985f85
Damien FREMONT <damien.fremont@gmail.com>
2026-05-06 21:14:34 +0200

    refactor: 💡 remove unused TileShader function and its associated TODO comment

--- platform/engine/include/eng_material.h
+++ platform/engine/include/eng_material.h
@@ -6,5 +6,4 @@
 // Module Functions Declaration
 //---------------------------------------------------------
 
-Shader TileShader(Vector2 tiling); // TODO: REMOVE ME
 Texture2D CheckboardTexture2D(Color col1, Color col2, float x, float y, float z);
--- platform/engine/src/eng_material.c
+++ platform/engine/src/eng_material.c
@@ -7,18 +7,6 @@
 // Module specific Functions Definition
 //---------------------------------------------------------
 
-// FIXME:
-Shader TileShader(Vector2 tiling) // TODO: REMOVEME
-{
-    // Set the texture tiling using a shader
-    const float tilingArr[2] = {tiling.x, tiling.y};
-    Shader shader = LoadShaderFile(
-        0,
-        "resources/shaders/glsl%i/tiling.fs");
-    SetShaderValue(shader, GetShaderLocation(shader, "tiling"), tilingArr, SHADER_UNIFORM_VEC2);
-    return shader;
-}
-
 Texture2D CheckboardTexture2D(Color col1, Color col2, float x, float y, float z) 
 {
     int checkByMeter = 1;
